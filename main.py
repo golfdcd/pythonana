42fb60a2f78b41d9750c5e4e5228bf04b36bcec3
golfdcd <68695900+golfdcd@users.noreply.github.com>
2020-09-18 15:09:19 +0700

    pythonana1

---
+++ main.py
@@ -0,0 +1,6 @@
+from sklearn import datasets
+iris_dataset = datasets.load_iris()
+#print(iris_dataset['data'])
+#print(iris_dataset['data'][0:10])
+#print(iris_dataset['target'])
+#print(iris_dataset['target_names'])
